3db838005131e0d0bc9457dde28daa12d81e9d2e
ApurvPatil306 <Apurv.Patil@pfizer.com>
2023-10-26 19:25:28 +0530

    test3

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -110,7 +110,7 @@ export default async function decorate(block) {
     const nav = document.createElement('nav');
     nav.id = 'nav';
     //const navPrimary = document.createElement('div');
-    
+    nav.innerHTML = html;
     
     const classes = ['brand', 'sections','secondary'];
     classes.forEach((c, i) => {
